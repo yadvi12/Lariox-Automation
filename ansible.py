a0f2e72e846837834912b74b7b755489284ea713
Yadvi Bhalla <60046892+yadvi12@users.noreply.github.com>
2021-01-27 11:26:58 +0530

    Add files via upload

--- ansible.py
+++ ansible.py
@@ -2,7 +2,7 @@
 import os
 import getpass
 # This function will set the color of text to blue.
-os.system("tput setaf 4")
+#os.system("tput setaf 4")
 def ansible():
 
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>                                             ANSIBLE AUTOMATION                                   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
@@ -44,13 +44,13 @@ def ansible():
                 p.close()
                 i =i + 1
             print("Ansible is successfully configured in your system, if you want to test it, run ansible all --list host command.")
-            input()
+            input("Press enter key to continue")
         elif int(op) ==2:
             os.system("clear")
             print("Please make sure that ansible and yum is configured in your system.") 
             print(""" 
                       Press 1 to configure load balancer for the web-servers
-                      Press 2 to configure docker 
+                      Press 2 to configure yum in your system
                       Press 3 to configure web-server(it will also configure yum repo)
                       Press 5 to exit
                   """)
@@ -79,9 +79,24 @@ def ansible():
 
                 os.system("ansible-playbook setup.yml")
                 print("Load-Balancer is been successfully configured in your system, you can try accessing the IP of your load-balancer.")
-                input() 
+                input("Press enter key to continue") 
             elif int(opt) == 2:
-                print("docker")
+                 yumc = input("In how many systems you wanna configure yum: ")
+                 l = 0
+                 s = "Enter IP "
+                 c = ": "
+                 while l < int(yumc):
+                    ip = input(s + str(l+1) + c)
+                    user = input("Enter username: ")
+                    pwd = getpass.getpass("Enter password: ")
+                    p = open("/root/ip.txt", "a")
+                    p.write("[yumpreq]\n")
+                    p.write(ip + " ansible_user=" + user + " ansible_ssh_pass=" + pwd + " ansible_connection=ssh\n")
+                    p.close()
+                    l = l+1
+                 os.system("ansible-playbook yum.yml")
+                 print("Yum is successfully configured in your system")
+                 input("Press enter key to continue")
             elif int(opt) == 3:
                 nweb = input("how many systems you want to configure as web-servers: ")
                 i = 0
@@ -98,7 +113,7 @@ def ansible():
                     i = i+1
                 os.system("ansible-playbook web-preq-dynamic.yml")
                 print("Web-server is successfully configured on port 80")
-                input()
+                input("Press enter key to continue")
             elif int(opt) == 5:
                 exit()
 
--- task8.py
+++ task8.py
@@ -1,24 +1,32 @@
 import os
+#import pyttsx3
+
+#engine = pyttsx3.init()
+#voices = engine.getProperty('voices')
+#rate = engine.getProperty('rate')
+#engine.setProperty('rate', 125)
 
 
-from hadoop import *
+#from hadoop import *
 from LVMautomation import *
 from aws import *
-#from docker import *
+from docker import *
 from apache import *
 from ansible import *
 
 while True:
     os.system("clear")
-   
+    print(""" ************  LARIOX AUTOMATION  ************* """)
+ #   pyttsx3.speak("Greetings, this is Lariox, a personal voice assistant for linux based automated tasks.") 
     print("""    
     Press 1 for hadoop setup
     Press 2 for AWS 
     Press 3 to automate LVM Partitions
-    Press 4 to setup docker
+    Press 4 for docker configuration
     Press 5 to configure Apache webserver in your system
     Press 6 for ansible configuration
-    Press 7 to exit     
+    Press 7 to configure yum  
+    Press 8 to exit the program
     """)
 
     print()
@@ -36,6 +44,23 @@ while True:
         apache()
     elif int(option) == 6:
         ansible()
+    elif int(option) == 7:
+                 yumc = input("In how many systems you wanna configure yum: ")
+                 l = 0
+                 s = "Enter IP "
+                 c = ": "
+                 while l < int(yumc):
+                    ip = input(s + str(l+1) + c)
+                    user = input("Enter username: ")
+                    pwd = getpass.getpass("Enter password: ")
+                    p = open("/root/ip.txt", "a")
+                    p.write("[yumpreq]\n")
+                    p.write(ip + " ansible_user=" + user + " ansible_ssh_pass=" + pwd + " ansible_connection=ssh\n")
+                    p.close()
+                    l = l+1
+                 os.system("ansible-playbook yum.yml")
+                 print("Yum is successfully configured in your system")
+                 input("Press enter to continue")
     else:
         print("Thank you!")
         exit()
